ae75a8e1ef309f72084b0fb85c608a762f6eda87
Fernando Maldonado <fernando.maldonadob@ucuenca.edu.ec>
2021-11-03 22:08:37 -0500

    Completo el Hamburger Menu, Ajustes JavaScript. Se terminó la carpeta 9

--- shared.js
+++ shared.js
@@ -1,23 +1,46 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal')
+var modalNoButton = document.querySelector('.modal_action-negative')
 var vermasButton = document.querySelectorAll('.plataforma button');
-
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile_nav');
 //console.dir(backdrop);
 
-//backdrop.style.display = 'block';
-console.dir(vermasButton)
+//console.dir(backdrop.style['backgrund-image']);
+
 for (var i = 0; i < vermasButton.length; i++) {
     vermasButton[i].addEventListener('click',function(){
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        //modal.style.display = "block";
+        //backdrop.style.display = "block";
+        //modal.className = 'open'; //This actually overwrite the complete class list
+        modal.classList.add('open')
+        backdrop.classList.add('open')
     });
 }
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function() {
+    //mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open')
+    closemodal();
+});
 
-modalNoButton.addEventListener("click", closeModal);
-
-function closeModal (){
-    backdrop.style.display = "none"
-    modal.style.display = "none"
+if(modalNoButton) {
+    modalNoButton.addEventListener("click", closemodal);
 }
+
+function closemodal (){
+    //backdrop.style.display = "none"
+    //modal.style.display = "none"
+if(modal) {
+    modal.classList.remove('open')
+    }
+    
+    backdrop.classList.remove('open')
+}
+
+toggleButton.addEventListener("click",function() {
+    //mobileNav.style.display = 'block'
+    //backdrop.style.display = 'block'
+    mobileNav.classList.add('open')
+    backdrop.classList.add('open')
+});
